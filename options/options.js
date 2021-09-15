327dc2248827b0000e15035a22d508319534108a
Crowdbotics <team@crowdbotics.com>
2021-09-15 19:31:40 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,8 +1,13 @@
+const newStyles = null
 export const globalOptions = {
   name: "js_module_options_d_22908Identifier",
   url: "https://js_module_options_d_22908Identifier.botics.co",
   api: "https://js_module_options_d_22908Identifier.botics.co/api/v1"
 }
 export const modulesOptions = {
-  "@modules/app-menu": { title: "App Menu", copy: "Routes available" }
+  "@modules/app-menu": {
+    title: "App Menu #1",
+    copy: "Routes available #1",
+    styles: newStyles
+  }
 }
